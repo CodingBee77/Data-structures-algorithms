59e80ad086e38bfa44e541146ab01ac3a19c85ec
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-01 09:14:00 +0200

    Clean up the code

--- Arrays.py
+++ Arrays.py
@@ -57,10 +57,3 @@ print(heros)
 heros.sort()
 print(heros)
 
-#####################
-"""Create a list of all odd numbers between 1 and a max number.
-Max number is something you need to take from a user using input() function"""
-
-max_num = int(input("Give me a max number: "))
-list_odd = list(x for x in range(1, max_num) if not (x % 2 == 0))
-print(list_odd)
