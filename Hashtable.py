6a7f4a2606e3d39f53f24b7600fe0d34b2a8d2b0
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-01 11:27:03 +0200

    Exercises and files needed added

--- Hashtable.py
+++ Hashtable.py
@@ -1,3 +1,6 @@
+from typing import ClassVar
+
+
 class HashTable:
     def __init__(self):
         self.MAX = 100
@@ -33,19 +36,66 @@ class HashTable:
                 del self.arr[h][index]
 
 
-t = HashTable()
-t["march 6"] = 130
-t["march 17"] = 450
-t['march 8'] = 67
-t['march 9'] = 4
+# t = HashTable()
+# t["march 6"] = 130
+# t["march 17"] = 450
+# t['march 8'] = 67
+# t['march 9'] = 4
 
-print(t.arr)
-
-
-del t['march 9']
-print(t.arr)
-# print(t.get_hash("march 6"))
-# print(t.get_hash("march 109"))
 # print(t.arr)
+
+
+# del t['march 9']
+# print(t.arr)
+
+
+###############EXERCISES##################
+import csv
+
+nyc_weather = []
+with open('nyc_weather.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for line in csv_reader:
+        temp = line['temperature(F)']
+        nyc_weather.append(int(temp))
+
+"""
+What was the average temperature in first week of Jan?
+"""
+first_week_temp = nyc_weather[:6] 
+average = sum(first_week_temp)/len(first_week_temp)
+print(average)
+
+"""
+What was the maximum temperature in first week of Jan?
+"""
+maximum_temp = max(first_week_temp)
+print(maximum_temp)
+
+"""
+You have to read this file in python and print every word and its count as show below. 
+Think about the best data structure that you can use to solve this problem and figure out
+why you selected that specific data structure.
+"""
+##SOLUTION1-not good#######
+word_counter = {}
+with open('poem.txt') as file:
+    for line in file:
+        for word in line.split():
+            key = str(word)
+            if key in word_counter:
+                word_counter[key] += 1
+            else:
+                word_counter[key] = 1
+
+###SOLUTION2-better#####
+with open('poem.txt') as f:
+    words = f.read()
+    word_counter = {}
+    for word in words.replace(',', ' ').split():
+        word_counter[word] = word_counter.setdefault(word, 0) + 1
+print(word_counter)
+
+
 
 
