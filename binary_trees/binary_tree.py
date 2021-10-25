bad8625a9061ebf191782de80ba796b7cbe2d6c0
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-25 15:12:13 +0200

    Implement set of method to pre, in and post order traversal and find mix, max, calculate sum of nodes

--- binary_trees/binary_tree.py
+++ binary_trees/binary_tree.py
@@ -4,32 +4,30 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
-
     def add_child(self, data):
         if data == self.data:
             return
-        
+
         if data < self.data:
-            #add data in left subtree
-            if self.left: #if there is a leaf node then recursively call add_child method
+            # add data in left subtree
+            if self.left:  # if there is a leaf node then recursively call add_child method
                 self.left.add_child(data)
             else:
                 self.left = BinarySearchTreeNode(data)
         else:
-            #add data in right subtree
+            # add data in right subtree
             if self.right:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
 
-    
     def in_order_traversal(self):
         elements = []
 
         # visit left tree
         if self.left:
             elements += self.left.in_order_traversal()
-        
+
         # visit base node
         elements.append(self.data)
 
@@ -39,16 +37,86 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def post_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.post_order_traversal()
+
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    def pre_order_traversal(self):
+        elements = [self.data]
+
+        if self.left:
+            elements += self.left.pre_order_traversal()
+
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            # val might be in a left subtree
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            # val might be in a right subtree
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def find_min(self):
+        if not self.left:
+            return self.data
+
+        return self.left.find_min()
+
+    def find_max(self):
+
+        if not self.right:
+            return self.data
+        return self.right.find_max()
+
+    def calculate_sum(self, sum=0):
+        if self.left:
+            sum += self.left.calculate_sum(sum)
+
+        sum += self.data
+
+        if self.right:
+            sum += self.right.calculate_sum(sum)
+
+        return sum
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
 
-    for i in range(1,len(elements)):
+    for i in range(1, len(elements)):
         root.add_child(elements[i])
 
     return root
 
+
 if __name__ == '__main__':
     numbers = [17, 6, 7, 1, 20, 67, 53, 3]
     numbers_tree = build_tree(numbers)
-    print(numbers_tree.in_order_traversal())
+    # print(numbers_tree.in_order_traversal())
+    # print(numbers_tree.search(20))
+    # print(numbers_tree.find_max())
+    # print(numbers_tree.calculate_sum())
+    print(numbers_tree.post_order_traversal())
