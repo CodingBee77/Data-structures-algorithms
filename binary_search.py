07d94436e813f6091389c36229fa7851cd0fdc5e
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-11-02 16:11:55 +0100

    Add binary search recursive function

--- binary_search.py
+++ binary_search.py
@@ -1,10 +1,13 @@
+from util.util import time_it
+
+@time_it
 def linear_search(numbers_list, number_to_find):
     for index, element in enumerate(numbers_list):
         if element == number_to_find:
             return index
     return -1
 
-
+@time_it
 def binary_search(numbers_list, number_to_find):
     left_index = 0
     right_index = len(numbers_list) - 1
@@ -25,15 +28,50 @@ def binary_search(numbers_list, number_to_find):
     return -1
 
 
-     
+def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
+
+    if right_index < left_index:
+        return -1
+
+    mid_index = (left_index + right_index)//2
+
+    if mid_index >= len(numbers_list) or mid_index<0:
+        return -1
+
+    mid_number = numbers_list[mid_index]
+
+
+
+    if mid_number == number_to_find:
+        return mid_index
+
+    if mid_number< number_to_find:
+        left_index = mid_index + 1
+    else:
+        right_index = mid_index -1
+
+
+    return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
 
 
 if __name__ == '__main__':
-    numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
-    number_to_find = 45
+    # numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
+
+    # numbers_list = [i for i in range(1000001)]
+    # number_to_find = 1000000
 
     # index = linear_search(numbers_list, number_to_find)
     # print(f"Number found at index {index} using linear search")
-    index = binary_search(numbers_list, number_to_find)
-    print(f"Number found at index {index} using binary search")
+    # index = binary_search(numbers_list, number_to_find)
+    # print(f"Number found at index {index} using binary search")
+    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
+    print(f"Number found at index {index} using binary search recursive")
 
+
+    """
+linear_search  took  32.396793365478516 mil secq
+Number found at index 1000000 using linear search
+binary_search  took  0.0073909759521484375 mil sec
+Number found at index 1000000 using binary search
+
+    """
