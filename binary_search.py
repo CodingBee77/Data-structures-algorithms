004d8741e87913b2df3941f7e54002f9afddc310
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-11-02 16:56:09 +0100

    FInd all occurences of certain number

--- binary_search.py
+++ binary_search.py
@@ -1,11 +1,13 @@
 from util.util import time_it
 
+
 @time_it
 def linear_search(numbers_list, number_to_find):
     for index, element in enumerate(numbers_list):
         if element == number_to_find:
             return index
     return -1
+
 
 @time_it
 def binary_search(numbers_list, number_to_find):
@@ -14,16 +16,16 @@ def binary_search(numbers_list, number_to_find):
     mid_index = 0
 
     while left_index <= right_index:
-        mid_index = (left_index+ right_index)//2
+        mid_index = (left_index + right_index)//2
         mid_number = numbers_list[mid_index]
 
         if mid_number == number_to_find:
             return mid_index
 
-        if mid_number< number_to_find:
+        if mid_number < number_to_find:
             left_index = mid_index + 1
         else:
-            right_index = mid_index -1
+            right_index = mid_index - 1
 
     return -1
 
@@ -35,28 +37,49 @@ def binary_search_recursive(numbers_list, number_to_find, left_index, right_inde
 
     mid_index = (left_index + right_index)//2
 
-    if mid_index >= len(numbers_list) or mid_index<0:
+    if mid_index >= len(numbers_list) or mid_index < 0:
         return -1
 
     mid_number = numbers_list[mid_index]
 
-
-
     if mid_number == number_to_find:
         return mid_index
 
-    if mid_number< number_to_find:
+    if mid_number < number_to_find:
         left_index = mid_index + 1
     else:
-        right_index = mid_index -1
-
+        right_index = mid_index - 1
 
     return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
 
 
-if __name__ == '__main__':
-    # numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
+def find_all_occurences(numbers, number_to_find):
+    index = binary_search(numbers, number_to_find)
+    indices = [index]
 
+    i = index-1
+    while i >= 0:
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i - 1
+
+    i = index + 1
+    while i < len(numbers):
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i = i + 1
+
+    return sorted(indices)
+
+
+if __name__ == '__main__':
+    numbers_list = [1, 4, 6, 11, 15, 15, 15, 17, 21, 34, 34, 56]
+    number_to_find = 15
+# Measure time #
     # numbers_list = [i for i in range(1000001)]
     # number_to_find = 1000000
 
@@ -64,14 +87,11 @@ if __name__ == '__main__':
     # print(f"Number found at index {index} using linear search")
     # index = binary_search(numbers_list, number_to_find)
     # print(f"Number found at index {index} using binary search")
-    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
+    index = binary_search_recursive(
+        numbers_list, number_to_find, 0, len(numbers_list))
     print(f"Number found at index {index} using binary search recursive")
 
-
-    """
-linear_search  took  32.396793365478516 mil secq
-Number found at index 1000000 using linear search
-binary_search  took  0.0073909759521484375 mil sec
-Number found at index 1000000 using binary search
-
-    """
+# -------------------------
+    indexes = find_all_occurences(numbers_list, number_to_find)
+    print(
+        f"Number found at indexes {indexes} using binary search -find all occurences")
